b2efead47cab604c78a7c23cfaad5c5cb13fb6f3
Dima <dmitrymalko@gmail.com>
2024-06-06 19:51:46 +0300

    windowed tic-tac-toe finished

--- tic-tac-toe/tic-tac-toe.py
+++ tic-tac-toe/tic-tac-toe.py
@@ -8,55 +8,81 @@ window = Tk()
 window.title('tic-tac-toe')
 window.geometry(f'{WIDTH}x{HEIGHT}')
 
-field = [
-    ['*', '*', '*'],
-    ['*', '*', '*'],
-    ['*', '*', '*']
-]
-char = 'x'
+
+
+# field = [
+#     ['*', '*', '*'],
+#     ['*', '*', '*'],
+#     ['*', '*', '*']
+# ]
+
+
 turn = 1
 win_x = 0
 win_o = 0
 
-def show_field():
-    for row in range(3):
-        for column in range(3):
-            Button(
-                window, text=field[row][column],
-                command=lambda x=row, y=column: user_turn(x, y)
-                )\
-            .place(x=row * 100, y=column * 100 + 50, relwidth=0.33, relheight=0.3)
-
 def user_turn(x, y):
     global turn
-    if field[x][y] != '*': return
-    if turn % 2 == 0:
-        char = 'x'
-    else:
-        char = 'o'
+    if field[x][y]['text'] != '*': return
+    char = 'x' if turn % 2 == 0 else 'o'
 
-    field[x][y] = char
-    turn += 1
+    field[x][y]['text'] = char
 
-    show_field()
     if check_winer(x, y, char):
         showinfo('Victory', f'{char} wins')
+        add_points()
+        restart()
+        return
+    if turn == 9:
+        showinfo('Draw', 'Draw')
+        restart()
+    turn += 1
 
     
 def check_winer(row, column, char):
-    if field[row][0] == char and field[row][1] == char and field[row][2] == char:
+    if field[row][0]['text'] == char and field[row][1]['text'] == char and field[row][2]['text'] == char:
         return True
-    if field[0][column] == char and field[1][column] == char and field[2][column] == char:
+    if field[0][column]['text'] == char and field[1][column]['text'] == char and field[2][column]['text'] == char:
         return True
-    if field[0][0] == char and field[1][1] == char and field[2][2] == char:
+    if field[0][0]['text'] == char and field[1][1]['text'] == char and field[2][2]['text'] == char:
         return True
-    if field[0][2] == char and field[1][1] == char and field[2][0] == char:
+    if field[0][2]['text'] == char and field[1][1]['text'] == char and field[2][0]['text'] == char:
         return True
     return False
 
-show_field()
-label_x = Label(window, text=f'x: {win_x}', width=2).place(x=WIDTH//2-60, y=0)
-label_y = Label(window, text=f'o: {win_o}', width=2).place(x=WIDTH//2+40, y=0)
+def restart():
+    global turn
+    turn = 0
+    for row in range(3):
+        for column in range(3):
+            field[row][column]['text'] = '*'
+
+
+field = []
+for row in range(3):
+    field.append([])
+    for column in range(3):
+        button = Button(text = '*')
+        button['command'] = lambda x=row, y=column: user_turn(x, y)
+        button.place(x = column*100, y = row*100+30, relwidth=0.33, relheight=0.3)
+        field[row].append(button)
+
+
+
+label_x = Label(window, text=f'x: {win_x}', width=2)
+label_x.place(x=WIDTH//2-60, y=0)
+label_o = Label(window, text=f'o: {win_o}', width=2)
+label_o.place(x=WIDTH//2+40, y=0)
+
+def add_points():
+    global win_x
+    global win_o
+    if turn % 2 == 0:
+        win_x+=1
+        label_x['text'] = f'x: {win_x}'
+    else:
+        win_o+=1
+        label_o['text'] = f'o: {win_o}'
 
 btn_reset = Button(window)
 
